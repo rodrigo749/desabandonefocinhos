15ceb00b26314cb6e2c330b8bfb9cc532d1f5376
Diego Ortiz <ortizdtz@gmail.com>
2026-04-14 08:48:17 -0300

    icone

--- src/components/BottomNav/index.js
+++ src/components/BottomNav/index.js
@@ -120,7 +120,7 @@ export default function BottomNav() {
           />
         ))}
 
-        <div ref={accountButtonRef}>
+        <div ref={accountButtonRef} className={styles.navItemSlot}>
           <NavItem
             asButton
             label="Minha Conta"
